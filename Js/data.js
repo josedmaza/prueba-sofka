21a8ba518e0c897248ee523aa0bcebba44f67f15
josedmaza <josedmaza21@gmail.com>
2021-12-20 22:28:14 -0500

    Subiendo archivos

---
+++ Js/data.js
@@ -0,0 +1,221 @@
+export {categories};
+
+
+let categories = {
+    animales: {
+      data: [
+        {
+          question: "¿Qué es la encapsulación?",
+          options: [
+            "A: La encapsulación es una característica del objeto y guarda todos los datos ocultos.",
+            "B: Meter un objeto en una capsula.",
+            "C: la capacidad de crear clases que adquieren de manera automática los atributos y métodos de otras clases que ya existen.",
+            "D: A y C son correctas.",
+          ],
+          ans: "A: La encapsulación es una característica del objeto y guarda todos los datos ocultos.",
+        },
+        {
+          question: "¿Cual de las siguientes es una libreria de Javascript?",
+          options: ["A: Boostrap", "B: Angular", "C: React", "D: Tailwhind"],
+          ans: "C: React",
+        },
+        {
+          question: "Cual es un framework de CSS",
+          options: ["A: Sass ", "B: Bootstrap", "C: Moderna", "D: Phizer"],
+          ans: "B: Bootstrap",
+        },
+        {
+          question: "Quien es el creador de Python",
+          options: [
+            "A:Guido van Rossium",
+            "B:Brendan Eich ",
+            "C: Google",
+            "D: Cristiano Ronaldo",
+          ],
+          ans: "A:Guido van Rossium",
+        },
+        {
+          question: "Que es un objeto en Javascript",
+          options: [
+            "A: Una instancia de una clase",
+            "B: Una funcion",
+            "C: Una variable",
+            "D: Una clase",
+          ],
+          ans: "A: Una instancia de una clase",
+        },
+        {
+          question: "Que es un ciclo de control en Javascript",
+          options: [
+            "A: Una funcion",
+            "B: Control para toma de decisiones de cómputo",
+            "C: todo lo relacionado con la programación",
+            "D: Hace parte de la programacion funcional ",
+          ],
+          ans: "B: Control para toma de decisiones de cómputo",
+        },
+        {
+          question: "Que es una base de datos",
+          options: [
+            "A: Es un servidor que permite persistir información o datos concretos",
+            "B: Es un grupo de datos",
+            "C: Es un reporte de datos",
+            "D: Es una coleccion de datos o informacion",
+          ],
+          ans: "A: Es un servidor que permite persistir información o datos concretos",
+        },
+        {
+          question:
+            "Cual de los siguientes es un lenguaje de programacion compilado",
+          options: ["A: Javascript", "B: Html", "C: Python", "D: Java"],
+          ans: "D: Java",
+        },
+        {
+          question: "¿Qué lenguajes usamos más al lado del navegador?",
+          options: ["A: Java", "B: Javascript", "C: Ruby", "D: Python"],
+          ans: "A: Java",
+        },
+        {
+          question: "¿Qué es un algoritmo?",
+          options: [
+            "A: Conjunto de instrucciones",
+            "B: Codigo Fuente ",
+            "C: Instrucciones lógicas con un propósito específico",
+            "D: Son variables ",
+          ],
+          ans: "C: Instrucciones lógicas con un propósito específico",
+        },
+        {
+          question:
+            "Es un paradigma de programación orientado a un modelo abstracto de la realidad.",
+          options: [
+            "A: Oriendado a objetos",
+            "B: Orientado a eventos",
+            "C: Orientado a datos",
+            "D: Orientado a la programación funcional",
+          ],
+          ans: "A: Oriendado a objetos",
+        },
+        {
+          question:
+            "Un lenguaje que se usa para hacer consultas en una base de datos relacional.",
+          options: ["A: LSD", "B: Query", "C: Pl", "D: SQL"],
+          ans: "D: SQL",
+        },
+        {
+          question:
+            "Si quiero iterar o recorrer una colección de objetos, ¿qué puedo usar?",
+          options: ["A: do-while", "B: if-else", "C: for", "D: elif"],
+          ans: "C: for",
+        },
+        {
+          question: "Uno de los motores de bases de datos más conocidos es:",
+          options: ["A: MySQL", "B: Vbasic", "C: DataStrorage", "D: Excel"],
+          ans: "A: MySQL",
+        },
+        {
+          question: "¿Qué es el polimorfismo?",
+          options: [
+            "A: Es el concepto con el cual se aclara que una clase divide la estructura y comportamiento definidos en otra clase.",
+            "B: Es parte de una clase.",
+            "C: Es la asignación de un valor que se da a una subclase, o que se había declarado ya en la clase principal.",
+            "D: Es la representación de un cierto tipo de objeto.",
+          ],
+          ans: "C: Es la asignación de un valor que se da a una subclase, o que se había declarado ya en la clase principal.",
+        },
+        {
+          question: "¿Qué se puede hacer en una Subclase?",
+          options: [
+            "A: Los campos heredados se pueden usar directamente, al igual que cualquier otro campo.",
+            "B: Podemos declarar nuevos campos en la subclase que no están en la superclase.",
+            "C: Los métodos heredados se pueden usar directamente tal como son.",
+            "D: Todas son correctas.",
+          ],
+          ans: "D: Todas son correctas.",
+        },
+        {
+          question: "¿Que es la herencia?",
+          options: [
+            "A: Es la representación de un cierto tipo de objeto. Es el plan que define las características de un objeto.",
+            "B: Es el concepto con el cual se aclara que una clase divide la estructura y comportamiento definidos en otra clase.",
+            "C: Es la asignación de un valor que se da a una subclase, o que se había declarado ya en la clase principal.",
+            "D: Un objeto es parte de una clase. Tiene su propio estado, comportamiento y definición.",
+          ],
+          ans: "B: Es el concepto con el cual se aclara que una clase divide la estructura y comportamiento definidos en otra clase.",
+        },
+        {
+          question: "¿Que es una clase abstracta?",
+          options: [
+            "A: Es el método que se nombra de forma automática cuando el objeto está realizado de alcance o destruido.",
+            "B: Es una función parte de una clase",
+            "C: Es una función donde se aplican varios operadores y que depende de los argumentos.",
+            "D: Es aquella clase que no puede ser instanciada.",
+          ],
+          ans: "D: Es aquella clase que no puede ser instanciada.",
+        },
+        {
+          question:
+            "Cual no es una caracteristica de la programacion orientada a objetos",
+          options: [
+            "A: Herencia",
+            "B: Polimorfismo",
+            "C: Encapsulamiento",
+            "D: Testamento",
+          ],
+          ans: "D: Testamento",
+        },
+        {
+          question: "¿Qué entiende por redundancia de datos?",
+          options: [
+            "A: Es la duplicacion de datos en la base de datos.",
+            "B: Es una función parte de una clase.",
+            "C: Es aquella clase que no puede ser instanciada.",
+            "D: A y C son verdaderas.",
+          ],
+          ans: "A: Es la duplicacion de datos en la base de datos",
+        },
+        {
+          question: "Que es la normalizacion en una base de datos",
+          options: [
+            "A: Es el proceso de eliminar datos redundantes de la base de datos dividiendo la tabla de una manera bien definida para mantener la integridad de los datos.",
+            "B: Es cuando los datos se almacenan de una forma estructurada",
+            "C: Es una colección organizada de datos relacionados donde los datos se almacenan y organizan para cumplir un propósito específico.",
+            "D: A y C son verdaderas",
+          ],
+          ans: "A: es el proceso de eliminar datos redundantes de la base de datos dividiendo la tabla de una manera bien definida para mantener la integridad de los datos.",
+        },
+        {
+          question: "Las ventajas de SQL son:",
+          options: ["A:SQL es fácil de aprender y casi todos los DBMS admiten SQL.", 
+          "B: Todas son verdaderas",
+           "C: Se pueden utilizar consultas SQL simples para recuperar una gran cantidad de datos de la base de datos de manera muy rápida y eficiente.", 
+           "D: Es más fácil administrar la base de datos usando SQL ya que no se requiere una gran cantidad de codificación."],
+          ans: "B: Todas son verdaderas",
+        },
+        {
+          question: "Cuáles son las desventajas de una consulta",
+          options: ["A: Los índices no están presentes.", 
+          "B: Los procedimientos almacenados se compilan excesivamente.", 
+          "C: Dificultad para interactuar.", 
+          "D: Todas son verdaderas"],
+          ans: "D: Todas son verdaderas",
+        },
+        {
+          question: "¿Qué es la partición de bases de datos?",
+          options: ["A: es un conjunto de información que describe el contenido y la estructura de las tablas y los objetos de la base de datos.", 
+          "B: Un conjunto de comandos que se ejecutan automáticamente cuando ocurre un evento", 
+          "C: El particionamiento de la base de datos es el proceso de particionar tablas, índices en partes más pequeñas para administrar y acceder a los datos en un nivel más fino.", 
+          "D: Nunguna de las anteriores es correcta"],
+          ans: "C: El particionamiento de la base de datos es el proceso de particionar tablas, índices en partes más pequeñas para administrar y acceder a los datos en un nivel más fino.",
+        },
+        {
+          question: "¿Qué significan las siglas Php?",
+          options: ["A: Hypertext Preprocessor", 
+          "B: Hyper Presentation", 
+          "C: Hypertext Processor", 
+          "D: Nunguna de las anteriores es correcta"],
+          ans: "A: Hypertext Preprocessor",
+        },
+      ],
+    },
+  };
---
+++ Js/index.js
@@ -0,0 +1,141 @@
+import {categories} from "../Js/data.js";
+
+let jugadores = {
+  name: "",
+  score: 0,
+};
+
+// 1. Create a function that takes in a category name and returns the questions for that category.
+class game {
+  constructor(categories) {
+    this.categories = categories;
+    this.currentCategory = categories.animales;
+    this.currentQuestion = 0;
+    this.questionCount = 1;
+    this.score = 0;
+  }
+}
+
+let $document = document;
+let $game = $document.getElementById("game");
+let $start = $document.getElementById("start");
+let $score = $document.getElementById("score");
+let $questionCount = $document.getElementById("questionCount");
+let $restart = $document.getElementById("restart");
+let $submit = $document.getElementById("submit");
+let $question = $document.getElementById("question");
+let $option = $document.getElementById("option");
+let $optionA = $document.getElementById("optionA");
+let $optionB = $document.getElementById("optionB");
+let $optionC = $document.getElementById("optionC");
+let $optionD = $document.getElementById("optionD");
+let $categorySelect = $document.getElementById("categorySelect");
+let $playerName = $document.getElementById("playerName");
+
+
+
+  
+
+
+$start.addEventListener("click", function () {
+  $start.style.display = "none";
+  $game.style.display = "flex";
+});
+
+const player1 = new game(categories);
+let rand = Math.floor(Math.random()*25);
+$question.innerHTML =
+  player1.currentCategory.data[rand].question;
+$optionA.innerHTML =
+  player1.currentCategory.data[rand].options[0];
+$optionB.innerHTML =
+  player1.currentCategory.data[rand].options[1];
+$optionC.innerHTML =
+  player1.currentCategory.data[rand].options[2];
+$optionD.innerHTML =
+  player1.currentCategory.data[rand].options[3];
+
+let opcion;
+$optionA.addEventListener("click", function () {
+  opcion = $optionA.textContent;
+  $optionA.style.backgroundColor = "#00ff00";
+  $optionB.style.backgroundColor = "#ff0000";
+  $optionC.style.backgroundColor = "#ff0000";
+  $optionD.style.backgroundColor = "#ff0000";
+});
+$optionB.addEventListener("click", function () {
+  opcion = $optionB.textContent;
+  $optionA.style.backgroundColor = "#ff0000";
+  $optionB.style.backgroundColor = "#00ff00";
+  $optionC.style.backgroundColor = "#ff0000";
+  $optionD.style.backgroundColor = "#ff0000";
+});
+$optionC.addEventListener("click", function () {
+  opcion = $optionC.textContent;
+  $optionA.style.backgroundColor = "#ff0000";
+  $optionB.style.backgroundColor = "#ff0000";
+  $optionC.style.backgroundColor = "#00ff00";
+  $optionD.style.backgroundColor = "#ff0000";
+});
+$optionD.addEventListener("click", function () {
+  opcion = $optionD.textContent;
+  $optionA.style.backgroundColor = "#ff0000";
+  $optionB.style.backgroundColor = "#ff0000";
+  $optionC.style.backgroundColor = "#ff0000";
+  $optionD.style.backgroundColor = "#00ff00";
+});
+
+function resetGame() {
+  $optionA.style.backgroundColor = "#F4F7F9";
+  $optionB.style.backgroundColor = "#F4F7F9";
+  $optionC.style.backgroundColor = "#F4F7F9";
+  $optionD.style.backgroundColor = "#F4F7F9";
+  player1.currentQuestion = 0;
+  player1.score = 0;
+  player1.questionCount = 1;
+  $score.innerHTML = player1.score;
+  $questionCount.innerHTML = player1.currentQuestion + rand;
+  $question.innerHTML =
+    player1.currentCategory.data[rand].question;
+  $optionA.innerHTML =
+    player1.currentCategory.data[rand].options[0];
+  $optionB.innerHTML =
+    player1.currentCategory.data[rand].options[1];
+  $optionC.innerHTML =
+    player1.currentCategory.data[rand].options[2];
+  $optionD.innerHTML =
+    player1.currentCategory.data[rand].options[3];
+}
+
+$submit.addEventListener("click", function () {
+  if (player1.questionCount < 5) {
+    if (player1.currentCategory.data[rand].ans == opcion) {
+      player1.score += 100;
+      player1.questionCount++;
+      player1.currentQuestion++;
+      rand++;
+      $score.innerHTML = player1.score;
+      $questionCount.innerHTML = player1.questionCount;
+      $question.innerHTML =
+        player1.currentCategory.data[rand].question;
+      $optionA.innerHTML =
+        player1.currentCategory.data[rand].options[0];
+      $optionB.innerHTML =
+        player1.currentCategory.data[rand].options[1];
+      $optionC.innerHTML =
+        player1.currentCategory.data[rand].options[2];
+      $optionD.innerHTML =
+        player1.currentCategory.data[rand].options[3];
+      $optionA.style.backgroundColor = "#F4F7F9";
+      $optionB.style.backgroundColor = "#F4F7F9";
+      $optionC.style.backgroundColor = "#F4F7F9";
+      $optionD.style.backgroundColor = "#F4F7F9";
+    } else {
+      alert("END GAME");
+      resetGame();
+    }
+  } else {
+    alert("YOU WIN");
+    resetGame();
+  }
+});
